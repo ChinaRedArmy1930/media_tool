9280bca7e656a585b6b8b80b3eabc392143e0434
xiaoyuuyang <xiaoyuuyang@tencent.com>
2025-10-13 07:52:54 +0000

    传一版代码 未经过测试

--- src/main.rs
+++ src/main.rs
@@ -549,7 +549,9 @@ async fn main() -> anyhow::Result<()> {
 
         log::info!("stream {} output path: {:?}", stream_index, output_path);
 
-        match stream.parameters().medium() {
+        let medium_type = stream.parameters().medium();
+
+        match medium_type {
             ffmpeg_next::media::Type::Video | ffmpeg_next::media::Type::Audio => {
                 let avformat_wrapper_output =
                     AVFormatContextWrapper::new(&output_path, false, true);
@@ -562,19 +564,25 @@ async fn main() -> anyhow::Result<()> {
                         .context(format!("创建输出目录失败: {:?}", parent))?;
                 }
 
-                let mut output_audio_steam = output_with_custom_io
+                let mut output_stream = output_with_custom_io
                     .output_mut()
                     .add_stream(ffmpeg_next::encoder::find(stream.parameters().id()))
                     .context("添加流失败")?;
 
-                output_audio_steam.set_parameters(stream.parameters());
+                output_stream.set_parameters(stream.parameters());
 
                 output_with_custom_io
                     .output_mut()
                     .write_header()
                     .context("写入音频文件头失败")?;
 
-                if let Some(packets) = audio_packets.get_mut(&stream_index) {
+                let packets = if medium_type == ffmpeg_next::media::Type::Audio {
+                    audio_packets.get_mut(&stream_index)
+                } else {
+                    data_packets.get_mut(&stream_index)
+                };
+
+                if let Some(packets) = packets {
                     for p in packets {
                         p.set_stream(0);
                         p.write_interleaved(output_with_custom_io.output_mut())
@@ -588,7 +596,41 @@ async fn main() -> anyhow::Result<()> {
                     .context("写入音频文件尾失败")?;
             }
 
-            _ => {}
+            _ => {
+                let avformat_wrapper_output =
+                    AVFormatContextWrapper::new(&output_path, false, true);
+                let (self_output, writer) = avformat_wrapper_output.unwrap().into_output();
+
+                let mut output_with_custom_io = OutputWithCustomIO::new(self_output, writer);
+
+                if let Some(parent) = output_path.parent() {
+                    std::fs::create_dir_all(parent)
+                        .context(format!("创建输出目录失败: {:?}", parent))?;
+                }
+
+                let mut output_stream = output_with_custom_io
+                    .output_mut()
+                    .add_stream(ffmpeg_next::encoder::find(stream.parameters().id()))
+                    .context("添加流失败")?;
+
+                output_stream.set_parameters(stream.parameters());
+
+                if let Some(packets) = match medium_type {
+                    ffmpeg_next::media::Type::Data => data_packets.get_mut(&stream_index),
+                    ffmpeg_next::media::Type::Attachment => {
+                        attachment_packets.get_mut(&stream_index)
+                    }
+                    ffmpeg_next::media::Type::Subtitle => subtitle_packets.get_mut(&stream_index),
+                    ffmpeg_next::media::Type::Unknown => unknown_packets.get_mut(&stream_index),
+                    _ => None,
+                } {
+                    for p in packets {
+                        p.set_stream(0);
+                        p.write_interleaved(output_with_custom_io.output_mut())
+                            .context("写入失败")?;
+                    }
+                }
+            }
         }
     }
 
